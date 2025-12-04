b7177e1cb2ce9b1ef8883e6b63603528aa9afb4c
aniket-mahadik-tb <aniket.mahadik@techbulls.com>
2025-12-04 18:12:51 +0530

    added configs to deploy in sub-directory

--- src/App.tsx
+++ src/App.tsx
@@ -50,175 +50,184 @@ const App = () => (
             <div className="flex-1">
               <Routes>
                 <Route path="/" element={<Landing />} />
-            <Route
-              path="/onboarding/*"
-              element={
-                <div className="min-h-screen flex flex-col">
-                  <ProgressBar />
-                  <Routes>
-                    <Route path="mobile" element={<MobileVerification />} />
-                    <Route path="aadhaar" element={<AadhaarVerification />} />
-                    <Route path="pan" element={<PANVerification />} />
-                    <Route path="dob" element={<DateOfBirth />} />
-                    <Route path="otp" element={<OTPVerification />} />
-                    <Route path="address" element={<AddressConfirmation />} />
-                    <Route path="basic-details" element={<BasicDetails />} />
-                    <Route
-                      path="product-selection"
-                      element={<ProductSelection />}
-                    />
-                    <Route path="nominee" element={<NomineeAndTerms />} />
-                    <Route path="success" element={<AccountSuccess />} />
-                    <Route path="kyc-prompt" element={<KYCPrompt />} />
-                  </Routes>
-                </div>
-              }
-            />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-            <Route
-              path="/loan/*"
-              element={
-                <Routes>
-                  <Route
-                    path="mobile"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <MobileEntry />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="mobile-otp"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <MobileOTPVerify />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="aadhaar"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <AadhaarEntry />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="aadhaar-otp"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <AadhaarOTPVerify />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="pan"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <PANEntry />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="personal-details-review"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <PersonalDetailsReview />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="basic-details"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <LoanBasicDetails />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="eligibility"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <LoanEligibility />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="customise"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <CustomiseLoan />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="bank-details"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <BankDetails />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="video-kyc"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <VideoKYC />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="kyc-prompt"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <LoanKYCPrompt />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="kyc-success"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <KYCSuccess />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="final-sanction"
-                    element={
-                      <>
-                        <LoanProgressBar />
-                        <FinalSanction />
-                      </>
-                    }
-                  />
-                  <Route
-                    path="disbursal-processing"
-                    element={<DisbursalProcessing />}
-                  />
-                  <Route
-                    path="disbursal-success"
-                    element={<DisbursalSuccess />}
-                  />
-                  <Route path="dashboard" element={<LoanDashboard />} />
-                </Routes>
-              }
-            />
-            {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-              <Route path="*" element={<NotFound />} />
+                <Route
+                  path="/onboarding/*"
+                  element={
+                    <div className="min-h-screen flex flex-col">
+                      <ProgressBar />
+                      <Routes>
+                        <Route path="mobile" element={<MobileVerification />} />
+                        <Route
+                          path="aadhaar"
+                          element={<AadhaarVerification />}
+                        />
+                        <Route path="pan" element={<PANVerification />} />
+                        <Route path="dob" element={<DateOfBirth />} />
+                        <Route path="otp" element={<OTPVerification />} />
+                        <Route
+                          path="address"
+                          element={<AddressConfirmation />}
+                        />
+                        <Route
+                          path="basic-details"
+                          element={<BasicDetails />}
+                        />
+                        <Route
+                          path="product-selection"
+                          element={<ProductSelection />}
+                        />
+                        <Route path="nominee" element={<NomineeAndTerms />} />
+                        <Route path="success" element={<AccountSuccess />} />
+                        <Route path="kyc-prompt" element={<KYCPrompt />} />
+                      </Routes>
+                    </div>
+                  }
+                />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route
+                  path="/loan/*"
+                  element={
+                    <Routes>
+                      <Route
+                        path="mobile"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <MobileEntry />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="mobile-otp"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <MobileOTPVerify />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="aadhaar"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <AadhaarEntry />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="aadhaar-otp"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <AadhaarOTPVerify />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="pan"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <PANEntry />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="personal-details-review"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <PersonalDetailsReview />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="basic-details"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <LoanBasicDetails />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="eligibility"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <LoanEligibility />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="customise"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <CustomiseLoan />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="bank-details"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <BankDetails />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="video-kyc"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <VideoKYC />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="kyc-prompt"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <LoanKYCPrompt />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="kyc-success"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <KYCSuccess />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="final-sanction"
+                        element={
+                          <>
+                            <LoanProgressBar />
+                            <FinalSanction />
+                          </>
+                        }
+                      />
+                      <Route
+                        path="disbursal-processing"
+                        element={<DisbursalProcessing />}
+                      />
+                      <Route
+                        path="disbursal-success"
+                        element={<DisbursalSuccess />}
+                      />
+                      <Route path="dashboard" element={<LoanDashboard />} />
+                    </Routes>
+                  }
+                />
+                {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+                <Route path="*" element={<NotFound />} />
               </Routes>
             </div>
             <Footer />
