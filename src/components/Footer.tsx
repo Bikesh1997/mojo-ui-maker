cd0d9f34479c28e2ee2742b1ee8b87bb105d7cb1
bikesh verma <bikesh.verma13@gmail.com>
2025-12-04 17:31:48 +0530

    space

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import logoTechbulls from "../../public/logo-techbulls.svg";
 
 export const Footer = () => {
   return (
-    <footer className="w-full bg-[#f7f3f7] py-[8px] px-[8px] sm:py-[10px] sm:px-[10px] border-t border-gray-200">
+    <footer className="m-5 w-full bg-[#f7f3f7] py-[8px] px-[8px] sm:py-[10px] sm:px-[10px] border-t border-gray-200">
       <div
     className="max-w-5xl mx-auto px-4 py-3 sm:py-4 flex flex-col items-center justify-center gap-1"
   >
